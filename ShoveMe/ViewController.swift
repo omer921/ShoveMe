fb5b2f237ead8ba76fb0ac2306f1e45a63c21618
Omer921 <Omer921@gmail.com>
2015-06-28 19:34:52 +0700

    Initial Commit

---
+++ ShoveMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoveMe
+//
+//  Created by Omer Winrauke on 6/28/15.
+//  Copyright (c) 2015 Omer Winrauke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
