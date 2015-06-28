4213f8926e6a2b6ad57102546c173c79c95d1704
Omer921 <Omer921@gmail.com>
2015-06-28 19:51:58 +0700

    test change

--- ShoveMe/ViewController.swift
+++ ShoveMe/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var labelShove: UILabel!
+    @IBOutlet weak var shoveButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func shoveButtonPressed(sender: UIButton) {
+        labelShove.text = "You've Been Shoved";
+        labelShove.hidden = false;
+        shoveButton.backgroundColor = UIColor.whiteColor();
+        shoveButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal);
+        shoveButton.setTitle("Shoved", forState: UIControlState.Normal);
+    }
 
 }
 
